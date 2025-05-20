5add25f601dd2c7fccff16cd076e2f258d40fa02
TehhX <splashyfish100@hotmail.com>
2025-05-20 14:41:06 -0400

    GitControl::sync() now warns about files outside repo before pulling
    Fixes #5

--- src/GitControl.cpp
+++ src/GitControl.cpp
@@ -38,10 +38,6 @@ void GitControl::pull() {
 }
 
 void GitControl::push(bool warnNotInRepo) {
-    // Should allow either in repo or locally untracked, not just in repo
-    if (warnNotInRepo && UserFileControl::areAnyStatus(UserFileControl::Status::IN_LOCAL))
-        throw GitCtrlErr("Some files are not inside the repository. Proceed anyway?", GitCtrlErr::SOME_OUTSIDE);
-
     if (!isActive)
         throw GitCtrlErr("GitControl is not initialized.", GitCtrlErr::BAD_INIT);
 
@@ -55,6 +51,10 @@ void GitControl::push(bool warnNotInRepo) {
 }
 
 void GitControl::sync(bool warnNotInRepo) {
+    // Should allow either in repo or locally untracked, not just in repo
+    if (warnNotInRepo && UserFileControl::areAnyStatus(UserFileControl::Status::IN_LOCAL))
+        throw GitCtrlErr("Some files are not inside the repository. Proceed anyway?", GitCtrlErr::SOME_OUTSIDE);
+
     pull();
     push(warnNotInRepo);
 }
